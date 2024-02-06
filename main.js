5622c1faccace4e1e350eeb4451529a411f984a1
janFiedler1 <50682895+janFiedler1@users.noreply.github.com>
2024-02-06 00:02:28 -0500

    lesson1@3:49

---
+++ main.js
@@ -0,0 +1,45 @@
+let legalSqaures = []
+
+const boardSquares = document.getElementsByClassName("square");
+const pieces = document.getElementsByClassName("piece");
+const piecesImage = document.getElementsByClassName("img");
+
+function setupBoardSquares() {
+    for(let i=0; i<boardSquares.length; i++) {  //add an event listener for dragging pieces over sqaures and dropping them
+        boardSquares[i].addEventListener("dragover", allowDrop);
+        boardSquares[i].addEventListener("drop", drop);
+        let row = 8-Math.floor(i/8);      
+        let column = String.fromCharCode(97+(i%8));   //a-h
+        let square = boardSquares[i];
+        square.id = column+row;
+    }
+}
+
+function setUpPieces() {
+    for (let i=0; i<pieces.length; i++){
+        pieces[i].addEventListener("dragstart", drag);
+        pieces[i].setAttribute("draggable", true);
+        pieces[i].id = pieces[i].className.split(" ")[1]+pieces[i].parentElement.id;
+    }
+    for (let i=0; i<pieces.length; i++){
+        piecesImages[i].setAttribute("draggable", false);
+    }
+}
+
+function allowDrop(ev){
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    const piece = ev.target;
+    ev.dataTransfer.setData("text", piece.id);
+}
+
+function drop(ev){
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    const piece = document.getElementById(data);
+    const destinationSquare = ev.currentTarget;
+    let destinationSquareId = destinationSquare.id;
+    destinationSquare.appendChild(piece);
+}
